62f68ba7b8b2fb7a1951adbae69c2f0e16e176e3
Balineni Harsha Sai <167176992+Balineni789@users.noreply.github.com>
2025-01-21 17:17:24 +0530

    Add files via upload

---
+++ 31.Web-Page/website3.js
@@ -0,0 +1,19 @@
+
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
+document.getElementById('togglePassword').addEventListener('click', function () {
+    const passwordInput = document.getElementById('password');
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+    this.textContent = type === 'password' ? '👁️' : '🙈'; // Change icon based on visibility
+});
